ea8555da341c39ae030a89ed49813539f832bd5a
Hanna Liz <hannaliz@MacBook-Pro-de-Hanna.local>
2024-09-11 16:37:36 -0300

    feat: tests

--- src/contexts/CityContext.tsx
+++ src/contexts/CityContext.tsx
@@ -1,19 +1,25 @@
-import { ReactNode, createContext, useEffect, useState, } from 'react';
+import React, { ReactNode, createContext, useEffect, useState } from "react";
+import {
+  getStorageCity,
+  removeStorageCity,
+  saveStorageCity,
+} from "@libs/asyncStorage/cityStorage";
 
-import { CityProps } from '@services/getCityByNameService';
-import { getStorageCity, removeStorageCity, saveStorageCity } from '@libs/asyncStorage/cityStorage';
+import { CityProps } from "@services/getCityByNameService";
 
 type CityContextProviderProps = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
 type CityContextDataProps = {
   cityIsLoading: boolean;
   city: CityProps | null;
   handleChanceCity: (city: CityProps) => void;
-}
+};
 
-export const CityContext = createContext<CityContextDataProps>({} as CityContextDataProps);
+export const CityContext = createContext<CityContextDataProps>(
+  {} as CityContextDataProps
+);
 
 export function CityProvider({ children }: CityContextProviderProps) {
   const [cityIsLoading, setCityIsLoading] = useState(true);
@@ -37,12 +43,14 @@ export function CityProvider({ children }: CityContextProviderProps) {
   }, []);
 
   return (
-    <CityContext.Provider value={{
-      city,
-      cityIsLoading,
-      handleChanceCity
-    }}>
+    <CityContext.Provider
+      value={{
+        city,
+        cityIsLoading,
+        handleChanceCity,
+      }}
+    >
       {children}
     </CityContext.Provider>
-  )
+  );
 }
